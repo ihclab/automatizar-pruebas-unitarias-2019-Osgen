7ee9cdd8c519e16946db898166a5367de9e596db
0scar Vc <ovirgen0@ucol.mx>
2019-03-20 20:32:08 -0600

    Problema #004 : Convertir entradas y resultados en tipo de datos esperados -> Listo

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
 let Medias = require("./Medias");
 
 let test = new Medias();
-console.log(test.mediaAritmetica([2,2,5]));
-console.log(test.raizEnesima(64,3));
-console.log(test.mediaGeometrica([3,5,1]));
+// console.log(test.mediaAritmetica([2,2,5]));
+// console.log(test.raizEnesima(64,3));
+// console.log(test.mediaGeometrica([3,5,1]));
 
 //read file
 let fs = require("fs");
 fs.readFile("test/CasosPrueba.txt", function(err, buf) {
     //all commands in this line.
     let lines = buf.toString().split("\n");
+    console.log(lines);
+    
     lines.forEach(line =>{
       let arg = line.split(":");
-      console.log(arg);
+      
+      let params = arg[2].split(" ").map(Number);
+      let results = arg[3] = Number(arg[3]);
+      // console.log(results);
+      // console.log(arg);
       
     });
-    //console.log(lines);
   });
