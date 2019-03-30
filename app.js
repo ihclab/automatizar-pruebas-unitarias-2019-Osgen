d465313cab5d03be233fab9cd96bf1fdcd0888d8
0scar Vc <ovirgen0@ucol.mx>
2019-03-29 18:00:11 -0600

    Actividad #003 : Guardar resultados en un archivo de texto -> Listo

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 
 let Medias = require("./Medias");
+let moment = require('moment');//npm install moment
+let fs = require("fs");
 
+//Get Medias class
 let test = new Medias();
 
+let resultados=`ID   Resultado Metodo           Detalles
+==========================================================\n\n`;
+
 //read file
-let fs = require("fs");
 fs.readFile("test/CasosPrueba.txt", function(err, buf) {
     //all commands in this line.
     let lines = buf.toString().split("\n");
     let id =0001;
+    let exitos=0;
+    let fallas=0;
 
     lines.forEach(line =>{
       let inicio = new Date().getTime();
@@ -17,12 +24,17 @@ fs.readFile("test/CasosPrueba.txt", function(err, buf) {
       let params = arg[2].split(" ").map(Number);
       let results = arg[3] = Number(arg[3]);//Convertion return NaN if was null in string, validated in class Medias
 
+      let lineToWrite;
         try {
           let result = test[funct](params).toFixed(4);
           if(result==results){
-            console.log('\x1b[32m%s\x1b[0m', `00${id} Exito ${funct} Calculado = ${result} T.E. ${(new Date().getTime()-inicio).toFixed(5)}`);
+            lineToWrite = `00${id} \t Exito \t ${funct} Calculado = ${result} T.E. ${(new Date().getTime()-inicio).toFixed(5)}`;
+            console.log('\x1b[32m%s\x1b[0m', lineToWrite);
+            exitos++;
           }else{
-            console.log('\x1b[31m%s\x1b[0m', `00${id} Falla ${funct} Calculado = ${result} Esperado = ${results} T.E. ${(new Date().getTime()-inicio).toFixed(5)}`);
+            lineToWrite = `00${id} \t Falla \t ${funct} Calculado = ${result} Esperado = ${results} T.E. ${(new Date().getTime()-inicio).toFixed(5)}`;
+            console.log('\x1b[31m%s\x1b[0m', lineToWrite);
+            fallas++;
           }
         } catch (e) {
           let error;
@@ -31,9 +43,29 @@ fs.readFile("test/CasosPrueba.txt", function(err, buf) {
           }else{
             error= "Metodo no encontrado";
           }
-          console.log(`00${id} ${funct} ${error}`);
-        }  
+          lineToWrite = `00${id} \t \t \t${funct} ${error}`;
+          console.log(lineToWrite);
+        } 
+
         id++;
+        resultados +=`${lineToWrite}\n`;
+
+      });
+
+      resultados +=`\n\n========= Fin de la prueba ==========\n\n
+      Exitos = ${exitos} \t Fallas = ${fallas}`;
+      console.error(resultados);
+
+      let fecha = moment().add(10, 'days').calendar().split("/");
+      let hora = moment().format('LT').split(":");
+      console.log(hora);
+      
+      fs.writeFile(`test/${fecha[0]}-${fecha[1]}-${fecha[2]}-${hora[0]}-${hora[1]}.txt`, resultados, 'ascii', (err)=>{
+        if(err){
+          console.log(err);      
+      }
     });
   });
+  
+
   
